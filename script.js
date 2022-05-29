cec9dabf359f596fcae4f05e6ce48f384c39bf92
sudhanshu <sudoofe123@gmail.com>
2022-05-30 04:04:08 +0530

    changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const section = document.querySelector("section");
 
-document.querySelector(".nav__links").addEventListener("click", function (e) {
+document.querySelector(".nav-item").addEventListener("click", function (e) {
   e.preventDefault();
   console.log(e.target);
 
-  if (e.target.classList.contains("nav__link")) {
+  if (e.target.classList.contains("nav-link")) {
     console.log("link");
 
     const id = e.target.getAttribute("href");
@@ -15,6 +15,22 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     });
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
 const loading = document.querySelector("#loading");
 loading.addEventListener("click", function (e) {
@@ -29,152 +45,6 @@ loading.addEventListener("click", function (e) {
     });
   }
 });
-
-// particlesJS("particles-js", {
-//   particles: {
-//     number: { value: 80, density: { enable: true, value_area: 800 } },
-//     color: { value: "#ffffff" },
-//     shape: {
-//       type: "circle",
-//       stroke: { width: 0, color: "#000000" },
-//       polygon: { nb_sides: 5 },
-//       image: { src: "img/github.svg", width: 100, height: 100 },
-//     },
-//     opacity: {
-//       value: 0.5,
-//       random: false,
-//       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-//     },
-//     size: {
-//       value: 3,
-//       random: true,
-//       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-//     },
-//     line_linked: {
-//       enable: true,
-//       distance: 150,
-//       color: "#ffffff",
-//       opacity: 0.4,
-//       width: 1,
-//     },
-//     move: {
-//       enable: true,
-//       speed: 6,
-//       direction: "none",
-//       random: false,
-//       straight: false,
-//       out_mode: "out",
-//       bounce: false,
-//       attract: { enable: false, rotateX: 600, rotateY: 1200 },
-//     },
-//   },
-//   interactivity: {
-//     detect_on: "canvas",
-//     events: {
-//       onhover: { enable: true, mode: "repulse" },
-//       onclick: { enable: true, mode: "push" },
-//       resize: true,
-//     },
-//     modes: {
-//       grab: { distance: 400, line_linked: { opacity: 1 } },
-//       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-//       repulse: { distance: 200, duration: 0.4 },
-//       push: { particles_nb: 4 },
-//       remove: { particles_nb: 2 },
-//     },
-//   },
-//   retina_detect: true,
-// });
-// var count_particles, stats, update;
-// stats = new Stats();
-// stats.setMode(0);
-// stats.domElement.style.position = "absolute";
-// stats.domElement.style.left = "0px";
-// stats.domElement.style.top = "0px";
-// document.body.appendChild(stats.domElement);
-// count_particles = document.querySelector(".js-count-particles");
-// update = function () {
-//   stats.begin();
-//   stats.end();
-//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-//   }
-//   requestAnimationFrame(update);
-// };
-// requestAnimationFrame(update);
-
-// particlesJS("particles-js", {
-//   particles: {
-//     number: { value: 80, density: { enable: true, value_area: 800 } },
-//     color: { value: "#47d750" },
-//     shape: {
-//       type: "circle",
-//       stroke: { width: 3, color: "#47d750" },
-//       polygon: { nb_sides: 3 },
-//       image: { src: "img/github.svg", width: 100, height: 100 },
-//     },
-//     opacity: {
-//       value: 0.5202497198655353,
-//       random: true,
-//       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-//     },
-//     size: {
-//       value: 3,
-//       random: true,
-//       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-//     },
-//     line_linked: {
-//       enable: true,
-//       distance: 150,
-//       color: "#fff",
-//       opacity: 0.5202497198655353,
-//       width: 0.9604610212902188,
-//     },
-//     move: {
-//       enable: true,
-//       speed: 6,
-//       direction: "none",
-//       random: true,
-//       straight: false,
-//       out_mode: "out",
-//       bounce: false,
-//       attract: { enable: false, rotateX: 600, rotateY: 1200 },
-//     },
-//   },
-//   interactivity: {
-//     detect_on: "canvas",
-//     events: {
-//       onhover: { enable: true, mode: "repulse" },
-//       onclick: { enable: true, mode: "push" },
-//       resize: true,
-//     },
-//     modes: {
-//       grab: { distance: 400, line_linked: { opacity: 1 } },
-//       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-//       repulse: { distance: 200, duration: 0.4 },
-//       push: { particles_nb: 4 },
-//       remove: { particles_nb: 2 },
-//     },
-//   },
-//   retina_detect: false,
-// });
-// var count_particles, stats, update;
-// // stats = new Stats();
-// // stats.setMode(0);
-// // stats.domElement.style.position = "absolute";
-// // stats.domElement.style.left = "0px";
-// // stats.domElement.style.top = "0px";
-// document.body.appendChild(stats.domElement);
-// count_particles = document.querySelector(".js-count-particles");
-// update = function () {
-//   stats.begin();
-//   stats.end();
-//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-//   }
-//   requestAnimationFrame(update);
-// };
-// requestAnimationFrame(update);
 
 particlesJS("particles-js", {
   particles: {
